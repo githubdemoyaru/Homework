9c215983bc21c97109dea217589e7c363133c4b5
githubdemoyaru <31182074+githubdemoyaru@users.noreply.github.com>
2017-08-20 05:22:00 -0700

    add problem 2 solution

---
+++ 3.HyperPrimeNumbers.cpp
@@ -0,0 +1,68 @@
+// ѕотрачено около 30 минут на написание и около 30 минут на безуспешные попытки пон€ть, что не так.
+// –езультат проверки: неправильный ответ
+// ¬се мои тесты выдавали ожидаемый ("правильный") ответ
+
+//#include "stdafx.h"
+#include "math.h"
+#include <iostream>
+
+using namespace std;
+
+
+bool isPrimeNumber(int num)
+{
+	if (num == 2)
+		return true;
+	if (num % 2 == 0)
+		return false;
+	int root = sqrt(num) + 0.9;
+	for (int i = 3; i <= root; i++)
+	{
+		if ((num % i) == 0)
+		{
+			return false;
+		}
+	}
+	
+	return true;	
+}
+
+bool isHyperPrimeNumber(int num)
+{
+	do
+	{
+		if (!isPrimeNumber(num))
+		{
+			return false;
+		}
+		num = num/10;
+	} while (num);
+
+	return true;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	bool c = false;
+
+	for (int i = a; i <= b; i++)
+	{
+		if (isHyperPrimeNumber(i))
+		{
+			c = true
+			cout << i << " ";
+		}
+	}
+
+	if (!c)
+	{
+		cout << 0 << endl;
+	}
+
+    return 0;
+}
+
+
